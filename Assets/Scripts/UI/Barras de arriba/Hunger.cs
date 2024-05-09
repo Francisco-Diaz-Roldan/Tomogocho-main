28dd2420070471c7c47f149297d1e8832fb95358
Francisco-Diaz-Roldan <pacodiaz008@gmail.com>
2024-05-09 14:08:07 +0200

    Ahora al estar muerto la barra de hambre no se puede modificar
    Script de Hunger arreglado, ya si el moñeco está muerto la barra no sube ni baja. (SIUUUU)

--- Assets/Scripts/UI/Barras de arriba/Hunger.cs
+++ Assets/Scripts/UI/Barras de arriba/Hunger.cs
@@ -6,37 +6,33 @@ using UnityEngine.UI;
 public class Hunger : MonoBehaviour
 {
     #region Variables
-    [SerializeField]
-    private Image _hungryBar;
-    //private Coroutine _coroutine;
+    [SerializeField] private Image _hungryBar;
+    private PlayerDead _playerDead; // Referencia al script PlayerDead
     #endregion
 
     private void Start()
     {
-        // Para pausar el juego -> Time.timeScale = 0f; poner a 1 para reanudar
+        _playerDead = FindObjectOfType<PlayerDead>(); // Encuentra el componente PlayerDead en la escena
+        if (_playerDead == null) { return; }
 
-        // Llama repetidamente a UpdateBar con un intervalo de 1 segundo
-        InvokeRepeating(nameof(UpdateBar), 1f, 1f);
+        // Llama repetidamente a UpdateBar con un intervalo de 1 segundo si el jugador no está muerto
+        if (!_playerDead.IsDead) { InvokeRepeating(nameof(UpdateBar), 1f, 1f); }
     }
 
     // Alimenta a la criatura aumentando el porcentaje de hambre
     public void FeedCreature()
     {
-       _hungryBar.fillAmount = Mathf.Min(1f, _hungryBar.fillAmount + .1f);
+        if (!_playerDead.IsDead) { _hungryBar.fillAmount = Mathf.Min(1f, _hungryBar.fillAmount + .1f); }
     }
 
     // Actualiza la barra de hambre
     private void UpdateBar()
     {
-        _hungryBar.fillAmount -= .01f;
-        _hungryBar.fillAmount = Mathf.Max(_hungryBar.fillAmount, 0f);
-    }
-
-    // TODO: Adaptar el tiempo en el que se va bajando la barra y cómo ésta se va llenando
-    // TODO: Ver cómo hacer que el tiempo siga transcurriendo aunque la aplicación esté cerrada
-    // TODO: Hacer la lógica para almacenar los valores de la barra
-
-    private void PlayerDead(){
-    //Que se instancie un sprite de una lapida y que suene una musiquita de morición
+        // Disminuye el porcentaje de hambre solo si el jugador no está muerto
+        if (!_playerDead.IsDead)
+        {
+            _hungryBar.fillAmount -= .01f;
+            _hungryBar.fillAmount = Mathf.Max(_hungryBar.fillAmount, 0f);
+        }
     }
 }

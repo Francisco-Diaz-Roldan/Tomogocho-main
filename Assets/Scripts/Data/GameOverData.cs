d58c9be6bd8d8fd4a3a9840c79b728293e659045
Francisco-Diaz-Roldan <pacodiaz008@gmail.com>
2024-05-09 22:15:12 +0200

    Creados Script de PlayerData(Guardado de partida) y GameOverData(Menú GameOver)
    Creado Script de PlayerData para guardar los datos.
    Creado Script de GameOverData para pasarle los datos del tiempo de vida de la criatura.
    Eliminado Script de RoundedButton.

---
+++ Assets/Scripts/Data/GameOverData.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class GameOverData : MonoBehaviour
+{
+    [SerializeField] private TMP_Text _days;
+    [SerializeField] private TMP_Text _hours;
+    [SerializeField] private TMP_Text _minutes;
+    [SerializeField] private PlayerData _playerData;
+
+    private void OnEnable()
+    {
+        ConvertirSegundos(_playerData.Seconds);
+        //Necesito desactivar el panel que hay detrás de Juego nuevo ya que cuando pulso lo que hay detrás también se ve afectado
+    }
+
+    private void ConvertirSegundos(float segundos)
+    {
+        // 1 día equivale a 86400 segundos (24 horas * 60 minutos * 60 segundos)
+        const float segundosEnUnDia = 86400;
+        // 1 hora equivale a 3600 segundos (60 minutos * 60 segundos)
+        const float segundosEnUnaHora = 3600;
+        // 1 minuto equivale a 60 segundos
+        const float segundosEnUnMinuto = 60;
+
+        // Calcular días
+        float dias = Mathf.Floor(segundos / segundosEnUnDia);
+        // Calcular horas restantes después de restar los días
+        float horasRestantes = segundos % segundosEnUnDia;
+        // Calcular horas
+        float horas = Mathf.Floor(horasRestantes / segundosEnUnaHora);
+        // Calcular minutos restantes después de restar las horas
+        float minutosRestantes = horasRestantes % segundosEnUnaHora;
+        // Calcular minutos
+        float minutos = Mathf.Floor(minutosRestantes / segundosEnUnMinuto);
+
+        _days.text = $"Días: {dias.ToString()}";
+        _hours.text = $"Horas: {horas.ToString()}";
+        _minutes.text = $"Minutos: {minutos.ToString()}";
+    }
+
+    public void Salir()
+    {
+        _playerData.ResetValues(false); //Creo un PLater Data vacio
+        this.gameObject.SetActive(false);
+    }
+}
---
+++ Assets/Scripts/Data/PlayerData.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "PlayerData", menuName = "Data/SaveData")]
+
+public class PlayerData : ScriptableObject
+{
+    public float HungerPercent;
+    public float HapinessPercent;
+    public float SleepPercent;
+    public DateTime? StartLifeTime; //TODO para futura mejora que incluya la fecha en la que empecé la partida
+    public float Seconds;   //Para medir la cantidad de tiempo jugado
+
+
+    public void ResetValues(bool newGame = true)
+    {
+        if (newGame)
+        {
+            HungerPercent = 1f;
+            HapinessPercent = 1f;
+            SleepPercent = 1f;
+            StartLifeTime = DateTime.Now;
+        }
+        else
+        {
+            HungerPercent = 0f;
+            HapinessPercent = 0f;
+            SleepPercent = 0f;
+            StartLifeTime = null;
+        }
+        Seconds = 0f;
+    }
+
+
+    //Cuando le de a nueva partida hacer un PLayerData.ResetValues() porque necesita una referencia previa (Hacer otro script)
+    //Cuando le dé a jugar y Seconds = 0f; -> Hacer un ResetValues() para
+    //Los datos siempre están cargados -> CUando inicie la escena tengo que coger los percents y tengo que ver cómo guardar los segundos -> Hacer un script que sea un contador de tiempo que debe 
+    //Necesito los porcentajes cuando inicio partida para que cada barra se cargue, cuando guarde partida que vuelque los datos al playerData
+    //Necesito un script de un inokerepeating de cada segundo en el cual se incremente un segundo por cada segundo en el temporizador que lo coja del PLayerDataSeconds +=seconds;
+
+}

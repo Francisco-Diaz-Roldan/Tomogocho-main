8f4ad003e9fbeece086a144c0b0919ae3c39e4ac
Francisco-Diaz-Roldan <pacodiaz008@gmail.com>
2024-05-17 21:27:06 +0200

    Aumenta la velocidad de llegada de las caquitas conforme va pasando el tiempo.
    Aumenta la velocidad de llegada de las caquitas conforme va pasando el tiempo.

--- Assets/Scripts/MInigames/Ground/Scroll.cs
+++ Assets/Scripts/MInigames/Ground/Scroll.cs
@@ -1,13 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading;
 using UnityEngine;
 
 public class Scroll : MonoBehaviour
 {
-    private float scrollSpeed = 6.5f;
+    [SerializeField] private float _initialScrollSpeed = 5; 
+
+    private float _scrollSpeed;
+    private float _timer;
+
+
+    void Start()
+    {
+        _scrollSpeed = _initialScrollSpeed;
+    }
 
     void Update()
     {
-        transform.Translate(Vector2.left * scrollSpeed * Time.deltaTime);
+        transform.Translate(Vector2.left * _scrollSpeed * Time.deltaTime);
+        UpdateSpeed();
+    }
+
+    private void UpdateSpeed()
+    {
+        float speedDivider = 10f;
+        _timer += Time.deltaTime;
+        _scrollSpeed = _initialScrollSpeed + _timer / speedDivider;
     }
 }

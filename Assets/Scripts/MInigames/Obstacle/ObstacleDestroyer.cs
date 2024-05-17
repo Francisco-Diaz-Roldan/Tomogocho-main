d3a318c6c880550e0bec60b6464e047f3c002700
Francisco-Diaz-Roldan <pacodiaz008@gmail.com>
2024-05-17 22:52:42 +0200

    Arreglados colliders de las caquitas del minijuego
    Arreglados colliders de las caquitas del minijuego

--- Assets/Scripts/MInigames/Obstacle/ObstacleDestroyer.cs
+++ Assets/Scripts/MInigames/Obstacle/ObstacleDestroyer.cs
@@ -6,8 +6,6 @@ public class ObstacleDestroyer : MonoBehaviour
 {
     private void OnTriggerEnter2D(Collider2D collision)
     {
-       // Destroy(collision.gameObject);
          collision.gameObject.SetActive(false);
-
     }
 }

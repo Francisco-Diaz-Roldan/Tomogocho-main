38d305383dfdb0edada1995e32b9f124d77807f0
Francisco-Diaz-Roldan <pacodiaz008@gmail.com>
2024-05-18 04:05:45 +0200

    Guardado de datos con las prefs en ChromeMinigameScene
    Retoques en el diseño de ChromeMinigameScene.
    Ya se almacenan las puntuaciones en la escena con las Preferencias.

--- Assets/Scripts/MInigames/Score.cs
+++ Assets/Scripts/MInigames/Score.cs
@@ -6,8 +6,17 @@ using TMPro;
 public class Score : MonoBehaviour
 {
     [SerializeField] private TMP_Text _scoreText;
+    [SerializeField] private TMP_Text _highScoreText;
     private int _score;
     private float _timer;
+    private int _highScore;
+
+    private void Start()
+    {
+        // Recupera la puntuación más alta guardada
+        _highScore = PlayerPrefs.GetInt("HighScore", 0);
+        _highScoreText.text = string.Format("{0:00000}", _highScore);
+    }
 
     private void Update()
     {
@@ -18,8 +27,23 @@ public class Score : MonoBehaviour
     {
         int scorePerSeconds = 10;
         _timer += Time.deltaTime;
-        _score = (int)(_timer  * scorePerSeconds);
+        _score = (int)(_timer * scorePerSeconds);
         _score = Mathf.RoundToInt(_score / 10) * 10;
         _scoreText.text = string.Format("{0:00000}", _score);
+
+        if (_score > _highScore)
+        {
+            _highScore = _score;
+            PlayerPrefs.SetInt("HighScore", _highScore);
+            PlayerPrefs.Save();
+            _highScoreText.text = string.Format("{0:00000}", _highScore);
+        }
+    }
+
+    public void ResetHighScore()
+    {
+        PlayerPrefs.DeleteKey("HighScore");
+        _highScore = 0;
+        _highScoreText.text = string.Format("{0:00000}", _highScore);
     }
 }

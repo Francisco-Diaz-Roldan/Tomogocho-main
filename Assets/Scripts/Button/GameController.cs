98af193e28a68457cc697ec441a9d6cad2d603da
Francisco-Diaz-Roldan <pacodiaz008@gmail.com>
2024-05-18 03:26:07 +0200

    Deshecho lo relativo al panel Jugar de ChromeMinigameController y otros ajustes menores
    Deshecho lo relativo al panel Jugar de ChromeMinigameController.
    Cambiado texto Score a SCORE en ChromeMinigameController

--- Assets/Scripts/Button/GameController.cs
+++ Assets/Scripts/Button/GameController.cs
@@ -19,21 +19,43 @@ public class GameController : MonoBehaviour
         if (!_playerDead.IsDead)
         {
             Time.timeScale = 0f;
-            _panelHome.SetActive(true);//_panelHome es el menú de pausa
+            _panelHome.SetActive(true);
         }
     }
 
     public void Reanudar()
     {
         Time.timeScale = 1f;
-        _panelHome.SetActive(false);//_panelHome es el menú de pausa
+        _panelHome.SetActive(false);
     }
 
     public void GoToMainScene()
     {
-        // Para ir a la escena Principal
-        //TODO hacer que se almacenen los datos 
         Time.timeScale = 1f;
         SceneManager.LoadScene("MainScene");
+    }
+
+    public void GoToChromeMiniGameScene()
+    {
+        StartCoroutine(LoadSceneAndPause("ChromeMiniGameScene"));
+    }
+
+    // Método para cargar la escena y pausarla
+    private System.Collections.IEnumerator LoadSceneAndPause(string sceneName)
+    {
+        // Pauso la escena actual
+       // Time.timeScale = 0f;
+
+        // Cargo la nueva escena de forma asíncrona
+        AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(sceneName);
+
+        // Espero hasta que la escena se haya cargado completamente
+        while (!asyncLoad.isDone)
+        {
+            yield return null;
+        }
+
+        // Pauso la nueva escena una vez que se haya cargado
+        //Time.timeScale = 0f;
     }
 }
--- Assets/Scripts/MInigames/ChromeMinigameController.cs
+++ Assets/Scripts/MInigames/ChromeMinigameController.cs
@@ -3,9 +3,25 @@ using UnityEngine.SceneManagement;
 
 public class ChromeMinigameController : MonoBehaviour
 {
+
+    [SerializeField] GameObject _panelJugar;
+
     public void Restart()
     {
+        Time.timeScale = 1f;
         SceneManager.LoadScene("ChromeMinigameScene");
+
+    }
+
+    public void GoToGameScene()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("GameScene");
+    }
+
+    public void Jugar()
+    {
+        _panelJugar.SetActive(false);
         Time.timeScale = 1f;
     }
 }

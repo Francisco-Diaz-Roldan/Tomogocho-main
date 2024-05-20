6ae735a416ec0e73150024a0f882601270cd1de5
Francisco-Diaz-Roldan <pacodiaz008@gmail.com>
2024-05-20 12:36:41 +0200

    IA Tres en raya hecha
    IA Tres en raya hecha

--- Assets/Scripts/MInigames/Tres en raya/TresEnRaya.cs
+++ Assets/Scripts/MInigames/Tres en raya/TresEnRaya.cs
@@ -34,17 +34,99 @@ public class TresEnRaya : MonoBehaviour
             }
             else
             {
-                if (playerTurn == "X")
+                SwitchTurn();
+                if (playerTurn == "O")
                 {
-                    playerTurn = "O";
-                    currentSprite = oSprite;
-                }
-                else
-                {
-                    playerTurn = "X";
-                    currentSprite = xSprite;
+                    StartCoroutine(AIMove());
                 }
             }
+        }
+    }
+
+    IEnumerator AIMove()
+    {
+        yield return new WaitForSeconds(0.5f); // Espera medio segundo antes de hacer el movimiento
+        int index = GetAIMove();
+        if (index != -1)
+        {
+            buttonImages[index].sprite = currentSprite;
+            if (CheckWin())
+            {
+                Debug.Log(playerTurn + " wins!");
+                ResetBoard();
+            }
+            else
+            {
+                SwitchTurn();
+            }
+        }
+    }
+
+    int GetAIMove()
+    {
+        // Prioridad 1: Ganar si es posible
+        int winMove = FindWinningMove(currentSprite);
+        if (winMove != -1) return winMove;
+
+        // Prioridad 2: Bloquear al jugador si está a punto de ganar
+        Sprite opponentSprite = (currentSprite == xSprite) ? oSprite : xSprite;
+        int blockMove = FindWinningMove(opponentSprite);
+        if (blockMove != -1) return blockMove;
+
+        // Prioridad 3: Hacer el mejor movimiento posible
+        for (int i = 0; i < buttonImages.Length; i++)
+        {
+            if (buttonImages[i].sprite == null)
+            {
+                return i;
+            }
+        }
+        return -1; // No hay movimientos disponibles
+    }
+
+    int FindWinningMove(Sprite sprite)
+    {
+        int[,] winCombinations = new int[,]
+        {
+            {0, 1, 2}, {3, 4, 5}, {6, 7, 8}, // Horizontales
+            {0, 3, 6}, {1, 4, 7}, {2, 5, 8}, // Verticales
+            {0, 4, 8}, {2, 4, 6}             // Diagonales
+        };
+
+        for (int i = 0; i < winCombinations.GetLength(0); i++)
+        {
+            int a = winCombinations[i, 0];
+            int b = winCombinations[i, 1];
+            int c = winCombinations[i, 2];
+
+            if (buttonImages[a].sprite == sprite && buttonImages[b].sprite == sprite && buttonImages[c].sprite == null)
+            {
+                return c;
+            }
+            if (buttonImages[a].sprite == sprite && buttonImages[c].sprite == sprite && buttonImages[b].sprite == null)
+            {
+                return b;
+            }
+            if (buttonImages[b].sprite == sprite && buttonImages[c].sprite == sprite && buttonImages[a].sprite == null)
+            {
+                return a;
+            }
+        }
+
+        return -1;
+    }
+
+    void SwitchTurn()
+    {
+        if (playerTurn == "X")
+        {
+            playerTurn = "O";
+            currentSprite = oSprite;
+        }
+        else
+        {
+            playerTurn = "X";
+            currentSprite = xSprite;
         }
     }
 
